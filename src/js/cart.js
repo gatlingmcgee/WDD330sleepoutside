76834488a42b3bcd6d4befae849aa37efec39c6d
gatlingmcgee <rzman03@aol.com>
2024-11-08 15:58:57 -0600

    w1individualactivity

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,8 +1,21 @@
 import { getLocalStorage } from "./utils.mjs";
 
+/*
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+}
+  */
+
+function renderCartContents() {
+  const cartItems = getLocalStorage("so-cart");
+
+  //change cartitems into an arry, because before, it was a single object
+  //const itemsArray = Array.isArray(cartItems) ? cartItems : [cartItems];
+  const updatedCartItems = [cartItems];
+
+  const htmlItems = updatedCartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
 
