951c8a21b4319fd505bc7adca57ff14adc78b366
jeremy-po <jeremypoliver@hotmail.com>
2024-11-09 19:57:08 -0400

    Individual Activity W01: Add item to cart broken

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,7 +2,9 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  const newCartItems = Array.isArray(cartItems) ? cartItems : [cartItems];
+
+  const htmlItems = newCartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
 
