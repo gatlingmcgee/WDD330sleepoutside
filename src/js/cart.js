69f44e006827c5d1328e2b32dd1322c0f29752a0
AndrewJHalisky <ajhalisky@gmail.com>
2024-11-08 21:26:03 -0700

    Fixed the cartItems program

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,7 +2,8 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  const cart = [cartItems];
+  const htmlItems = cart.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
 
