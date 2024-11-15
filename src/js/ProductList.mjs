530ac80c392c952a9ec4c77b4976ca797f662aa6
gatlingmcgee <rzman03@aol.com>
2024-11-14 18:26:36 -0600

    updating#1

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,48 @@
+import { renderListWithTemplate } from "./utils.mjs";
+
+// product card template
+function productCardTemplate(product) {
+
+  // product_pages/marmot-ajax-3
+
+  return `
+    <li class="product-card">
+      <a href="product_pages/index.html?product=${product.Id}">
+        <img src="${product.Image}" alt="Image of ${product.Name}">
+        <h3 class="card__brand">${product.Brand.Name}</h3>
+        <h2 class="card__name">${product.NameWithoutBrand}</h2>
+        <p class="product-card__price">$${product.FinalPrice}</p>
+      </a>
+    </li>`
+}
+
+export default class ProductListing {
+  constructor(category, dataSource, listElement) {
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
+
+  async init() {
+    const list = await this.dataSource.getData();
+    this.renderList(list);
+  }
+
+  renderList(list) {
+    /*
+    this.listElement.innerHTML = "";
+
+    //const productCardsHtml = products.map(product => productCardTemplate(product));
+    const productCardsHtml = products.map((product) => productCardTemplate(product)).join("");
+
+    this.listElement.innerHTML = productCardsHtml;
+    */
+
+    /*const htmlStrings = list.map(productCardTemplate);
+    this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
+    */
+
+    renderListWithTemplate(productCardTemplate, this.listElement, list);
+  }
+ }
+
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+// importing the product data from the ProductData.mjs
+import ProductData from "./ProductData.mjs"
+import ProductListing from "./ProductList.mjs";
+
+const dataSource = new ProductData("tents");
+const listElement = document.querySelector(".product-list");
+const productListing = new ProductListing("tents", dataSource, listElement);
+
+productListing.init();
