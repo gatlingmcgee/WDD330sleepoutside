914e9140b2ee7dcf20f920cae2fcc9c3d3dbc502
jeremy-po <jeremypoliver@hotmail.com>
2024-11-16 17:07:20 -0400

    Add discount to product detail pages

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,21 +1,34 @@
 import { setLocalStorage } from "./utils.mjs";
 
 function productDetailsTemplate(product) {
-  return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
+  const suggestedRetailPrice = product.SuggestedRetailPrice || 0;
+  const listPrice = product.ListPrice || 0;
+  const discount = suggestedRetailPrice > 0 ? ((suggestedRetailPrice - listPrice) / suggestedRetailPrice * 100).toFixed(0) : 0;
+  const discountedPrice = listPrice.toFixed(2); 
+
+  let priceHTML = `<p class="product-card__price">$${listPrice}</p>`;
+  let discountHTML = '';
+
+  
+  if (discount > 0) {
+    priceHTML = `<p class="product-card__price">
+                  <span class="original-price" style="text-decoration: line-through; color: gray;">$${suggestedRetailPrice}</span>
+                  <span class="discounted-price">$${discountedPrice}</span>
+                </p>`;
+    discountHTML = `<p class="discount-percentage">-${discount}% OFF</p>`; 
+  }
+
+  return `<section class="product-detail">
+    <h3>${product.Brand.Name}</h3>
     <h2 class="divider">${product.NameWithoutBrand}</h2>
-    <img
-      class="divider"
-      src="${product.Image}"
-      alt="${product.NameWithoutBrand}"
-    />
-    <p class="product-card__price">$${product.FinalPrice}</p>
+    <img class="divider" src="${product.Image}" alt="${product.NameWithoutBrand}" />
+    ${priceHTML} ${discountHTML}
     <p class="product__color">${product.Colors[0].ColorName}</p>
-    <p class="product__description">
-    ${product.DescriptionHtmlSimple}
-    </p>
+    <p class="product__description">${product.DescriptionHtmlSimple}</p>
     <div class="product-detail__add">
       <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
-    </div></section>`;
+    </div>
+  </section>`;
 }
 
 export default class ProductDetails {
