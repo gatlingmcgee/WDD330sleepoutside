43746a3639a8fd62705da7e03047d881085d83cd
gatlingmcgee <rzman03@aol.com>
2024-11-20 20:24:22 -0600

    individualchange#1

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -9,14 +9,13 @@ function productDetailsTemplate(product) {
       alt="${product.NameWithoutBrand}"
     />
     <p class="product-card__price">$${product.FinalPrice}</p>
-    <img class="divider" src="${product.Image}" alt="${product.NameWithoutBrand}" />
-    ${priceHTML} ${discountHTML}
     <p class="product__color">${product.Colors[0].ColorName}</p>
-    <p class="product__description">${product.DescriptionHtmlSimple}</p>
+    <p class="product__description">
+    ${product.DescriptionHtmlSimple}
+    </p>
     <div class="product-detail__add">
       <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
-    </div>
-  </section>`;
+    </div></section>`;
 }
 
 export default class ProductDetails {
@@ -27,8 +26,12 @@ export default class ProductDetails {
   }
 
   async init() {
+    // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
     this.product = await this.dataSource.findProductById(this.productId);
+    // once we have the product details we can render out the HTML
     this.renderProductDetails("main");
+    // once the HTML is rendered we can add a listener to Add to Cart button
+    // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
     document
       .getElementById("addToCart")
       .addEventListener("click", this.addToCart.bind(this));
