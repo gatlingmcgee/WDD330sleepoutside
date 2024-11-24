bb663fc85accc26257d5da4cea9cf3d475df8bdf
EChlarson <chl20001@byui.edu>
2024-11-23 19:14:42 -0600

    corrected double quotes in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,14 @@
 import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
-import Alert from './alert.js';
+import Alert from "./alert.js";
 
 const dataSource = new ProductData("tents");
 const listElement = document.querySelector(".product-list");
 const productListing = new ProductListing("tents", dataSource, listElement);
 
-const alert = new Alert('/json/alerts.json');
+const alert = new Alert("/json/alerts.json");
 
-alert.loadAlerts('maintenance');
+alert.loadAlerts("maintenance");
 loadHeaderFooter();
 productListing.init();
