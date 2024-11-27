17fed0062fdf15e39bf7ccbd8105ebd71453b507
AndrewJHalisky <ajhalisky@gmail.com>
2024-11-26 22:38:12 -0500

    Searchbar in progress

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,5 +1,5 @@
 const baseURL = import.meta.env.VITE_SERVER_URL
-
+// const baseURL = 'http://server-nodejs.cit.byui.edu:3000/'
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -17,11 +17,11 @@ export default class ProductData {
   }
     */
 
-async getData(category) {
+  async getData(category) {
     const response = await fetch(baseURL + `products/search/${category}`);
     const data = await convertToJson(response);
     return data.Result;
-}
+  }
 
   async findProductById(id) {
     const response = await fetch(baseURL + `product/${id}`);
@@ -29,3 +29,64 @@ async getData(category) {
     return data.Result;
   }
 }
+
+export async function searchTents() {
+  var input, filter, ul, li, i, a, i, txtValue;
+  input = document.getElementById("myInput")
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("list");
+  li = ul.getElementsByTagName('li');
+
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+// May not use this:
+// const products = [
+//   { product : '989CH'},
+//   { product : '880RT'},
+//   { product : '880RR'},
+//   { product : '985MD'},
+//   { product : '344YK'},
+//   { product : '344YJ'},
+//   { product : '989CG'},
+//   { product : '985KU'},
+//   { product : '15UHG'},
+//   { product : '15UGY'},
+//   { product : '543DT'},
+//   { product : '20DRU'},
+//   { product : '14GVF'},
+//   { product : '14GVH'},
+//   { product : '861PY'},
+//   { product : '14GVG'}
+// ]
+// const searchInput = document.querySelector(".input");
+
+// searchInput.addEventListener("input", (e) => {
+//   let value = e.target.value
+//   if (value && value.trim().length > 0) {
+//     value = value.trim().toLowerCase()
+//     setList(products.filter(name =>{
+//       return name.product.includes(value);
+//     }))
+//   } else {
+//     console.error("Item is null");
+//   }
+// })
+
+// async function setList(results) {
+//   for (const item of results) {
+//     const resultItem = document.createElement('p');
+//     resultItem.classList.add('result-item');
+//     const text = document.createTextNode(item.product)
+//     resultItem.appendChild(text)
+//     list.appendChild(resultItem)
+//   }
+// }
--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,13 @@
 import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
+import { searchTents } from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 const listElement = document.querySelector(".product-list");
 const productListing = new ProductListing("tents", dataSource, listElement);
 
 loadHeaderFooter();
+searchTents();
 
 productListing.init();
