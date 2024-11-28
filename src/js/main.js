52ea1efa560fa1143506fcb3f2ab04dfcf6554e9
EChlarson <chl20001@byui.edu>
2024-11-27 19:56:00 -0600

    merge conflict

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,14 @@
 // importing the product data from the ExternalServices.mjs
 import ExternalServices from "./ExternalServices.mjs";
 import ProductListing from "./ProductList.mjs";
-<<<<<<< HEAD
 import { loadHeaderFooter } from "./utils.mjs";
 import { searchTents } from "./ExternalServices.mjs";
-=======
-import {loadHeaderFooter  } from "./utils.mjs";
 import Alert from "./alert.js";
->>>>>>> 8bdc6c09a383cdbfe18969344a8cb72517094618
 
 const dataSource = new ExternalServices("tents");
 const listElement = document.querySelector(".product-list");
 const productListing = new ProductListing("tents", dataSource, listElement);
 
-<<<<<<< HEAD
-loadHeaderFooter();
-searchTents();
-
-productListing.init();
-=======
 const alert = new Alert("/json/alerts.json");
 
 document.getElementById("newsletterForm").addEventListener("submit", function(event) {
@@ -33,4 +23,4 @@ document.getElementById("newsletterForm").addEventListener("submit", function(ev
 alert.loadAlerts("maintenance");
 loadHeaderFooter();
 productListing.init();
->>>>>>> 8bdc6c09a383cdbfe18969344a8cb72517094618
+searchTents();
