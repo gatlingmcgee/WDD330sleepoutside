4959a795497000a980c1ab2e90de25a48bd72762
EChlarson <chl20001@byui.edu>
2024-11-27 20:14:57 -0600

    fix merge conflict

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -34,6 +34,24 @@ export default class ExternalServices {
   }
 }
 
+export async function searchTents() {
+  var input, filter, ul, li, i, a, txtValue;
+  input = document.getElementById("myInput")
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("list");
+  li = ul.getElementsByTagName("li");
+ 
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
 // May not use this:
 // const products = [
 //   { product : '989CH'},
