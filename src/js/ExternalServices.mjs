a236eb09c6302f21a27ce052cb77a31ee594a3bc
AndrewJHalisky <ajhalisky@gmail.com>
2024-11-30 12:58:46 -0500

    Added checkout.js on my end

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,7 +1,59 @@
-async function convertToJson(res) {
-    if (res.ok) {
-      return res.json();
+const baseURL = import.meta.env.VITE_SERVER_URL
+
+function convertToJson(res) {
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw new Error("Bad Response");
+  }
+}
+
+
+export default class ExternalServices {
+  /*
+  constructor(category) {
+    this.category = category;
+    this.path = `../json/${this.category}.json`;
+  }
+    */
+
+async getData(category) {
+    const response = await fetch(baseURL + `products/search/${category}`);
+    const data = await convertToJson(response);
+    return data.Result;
+}
+
+  async findProductById(id) {
+    const response = await fetch(baseURL + `product/${id}`);
+    const data = await convertToJson(response);
+    return data.Result;
+  }
+
+  async checkout(payload) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    };
+    return await fetch(baseURL + "checkout/", options).then(convertToJson);
+  }
+}
+
+export function searchTents() {
+  const input = document.getElementById("myInput");
+  const filter = input.value.toUpperCase();
+  const ul = document.getElementById("list");
+  const li = ul.getElementsByTagName("li");
+
+  for (let i = 0; i < li.length; i++) {
+    const a = li[i].getElementsByTagName("a")[0];
+    const txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "block";
     } else {
-      throw new Error('Bad Response');
+      li[i].style.display = "none";
     }
   }
+}
