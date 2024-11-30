dd7ae579aec6f374716281fcd379088b047f51c3
gatlingmcgee <rzman03@aol.com>
2024-11-29 18:28:18 -0600

    added the search branch

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -40,3 +40,20 @@ async getData(category) {
     return await fetch(baseURL + "checkout/", options).then(convertToJson);
   }
 }
+
+export function searchTents() {
+  const input = document.getElementById("myInput");
+  const filter = input.value.toUpperCase();
+  const ul = document.getElementById("list");
+  const li = ul.getElementsByTagName("li");
+
+  for (let i = 0; i < li.length; i++) {
+    const a = li[i].getElementsByTagName("a")[0];
+    const txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "block";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
