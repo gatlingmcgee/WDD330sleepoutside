b37761e3c3e1413fc6244066585a9d1130e8909d
AndrewJHalisky <ajhalisky@gmail.com>
2024-11-30 13:19:06 -0500

    Made more individual changes

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,40 +1,78 @@
-import { renderListWithTemplate } from "./utils.mjs";
+import { getParam, renderListWithTemplate } from "./utils.mjs";
+import { searchTents } from "./ExternalServices.mjs";
 
 function productCardTemplate(product) {
   return `<li class="product-card">
-  <a href="product_pages/index.html?product=${product.Id}">
-  <img
-    src="${product.Image}"
-    alt="Image of ${product.Name}"
-  />
-  <h3 class="card__brand">${product.Brand.Name}</h3>
-  <h2 class="card__name">${product.Name}</h2>
-  <p class="product-card__price">$${product.FinalPrice}</p></a>
-</li>`;
+    <a href="/product_pages/index.html?product=${product.Id}">
+      <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}" loading="lazy" />
+      <h3 class="card__brand">${product.Brand.Name}</h3>
+      <h2 class="card__name">${product.Name}</h2>
+      <p class="product-card__price">${product.FinalPrice}</p>
+      ${
+        product.FinalPrice < product.SuggestedRetailPrice
+          ? `<p class="discount">${Math.round(
+              ((product.SuggestedRetailPrice - product.FinalPrice) / product.SuggestedRetailPrice) * 100
+            )}% OFF</p>`
+          : ""
+      }
+    </a>
+  </li>`;
 }
 
-export default class ProductList {
+export default class ProductListing {
   constructor(category, dataSource, listElement) {
-    // We passed in this information to make our class as reusable as possible.
-    // Being able to define these things when we use the class will make it very flexible
     this.category = category;
     this.dataSource = dataSource;
     this.listElement = listElement;
+    this.products = []; // Array to store products
   }
+  
+
   async init() {
-    // our dataSource will return a Promise...so we can use await to resolve it.
-    const list = await this.dataSource.getData();
-    // render the list
-    this.renderList(list);
+    try {
+        const list = await this.dataSource.getData(this.category);
+        this.renderList(list);
+        this.products = list; // Save the products data
+        this.addSortEventListener();
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
   }
-  // render after doing the first stretch
+
   renderList(list) {
     renderListWithTemplate(productCardTemplate, this.listElement, list);
   }
 
-  // render before doing the stretch
-  // renderList(list) {
-  //   const htmlStrings = list.map(productCardTemplate);
-  //   this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
-  // }
+  addSortEventListener() {
+    const sortDropdown = document.getElementById("sort");
+    sortDropdown.addEventListener("change", (event) => {
+      this.sortList(event.target.value);
+    });
+  }
+
+  sortList(criteria) {
+    let sortedList;
+
+    switch (criteria) {
+      case "name-asc":
+        sortedList = this.products.sort((a, b) => a.Name.localeCompare(b.Name));
+        break;
+      case "name-desc":
+        sortedList = this.products.sort((a, b) => b.Name.localeCompare(a.Name));
+        break;
+      case "price-asc":
+        sortedList = this.products.sort((a, b) => a.FinalPrice - b.FinalPrice);
+        break;
+      case "price-desc":
+        sortedList = this.products.sort((a, b) => b.FinalPrice - a.FinalPrice);
+        break;
+      default:
+        sortedList = this.products;
+        break;
+    }
+
+    // Re-render the sorted list
+    this.renderList(sortedList);
+  }
+  
 }
