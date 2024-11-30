907a1823d2165dc460e50602795cabba5c8e50f8
AndrewJHalisky <ajhalisky@gmail.com>
2024-11-30 13:13:12 -0500

    Made more changes on individual branch

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,4 +1,4 @@
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter, getParam } from "./utils.mjs";
 //import { getParam } from "./utils.mjs";
