6aa198d0522b77768f963c54a52d0493609cf59d
AndrewJHalisky <ajhalisky@gmail.com>
2024-11-30 20:32:17 -0500

    Changes to External Services

--- src/js/product.js
+++ src/js/product.js
@@ -2,7 +2,7 @@ import { getParam } from "./utils.mjs";
 import ExternalServices from "./ExternalServices.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
-const dataSource = new ProductData("tents");
+const dataSource = new ExternalServices("tents");
 const productId = getParam("product");
 
 const product = new ProductDetails(productId, dataSource);
