5199fb9e51b40208b116b78ce73b8cebf7f65fc5
jeremy-po <jeremypoliver@hotmail.com>
2024-11-30 23:10:25 -0400

    W04 Individual Activity

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -1,4 +1,9 @@
-import { getLocalStorage } from "./utils.mjs";
+import {
+  setLocalStorage,
+  getLocalStorage,
+  alertMessage,
+  removeAllAlerts,
+} from "./utils.mjs";
 import ExternalServices from "./ExternalServices.mjs";
 
 const services = new ExternalServices();
@@ -20,6 +25,7 @@ function packageItems(items) {
       id: item.Id,
       price: item.FinalPrice,
       name: item.Name,
+      quantity: 1,
     };
   });
   return simplifiedItems;
@@ -38,18 +44,17 @@ export default class CheckoutProcess {
   init() {
     this.list = getLocalStorage(this.key);
     this.calculateItemSummary();
-    this.calculateOrdertotal();
   }
   calculateItemSummary() {
     const summaryElement = document.querySelector(
       this.outputSelector + " #cartTotal"
     );
     const itemNumElement = document.querySelector(
-      this.outputSelector + " #quantity"
+      this.outputSelector + " #num-items"
     );
     itemNumElement.innerText = this.list.length;
     // calculate the total of all the items in the cart
-    const amounts = this.list.map((item) => item.FinalPrice * item.quantity);
+    const amounts = this.list.map((item) => item.FinalPrice);
     this.itemTotal = amounts.reduce((sum, item) => sum + item);
     summaryElement.innerText = "$" + this.itemTotal;
   }
@@ -87,7 +92,15 @@ export default class CheckoutProcess {
     try {
       const res = await services.checkout(json);
       console.log(res);
+      setLocalStorage("so-cart", []);
+      location.assign("/checkout/success.html");
     } catch (err) {
+      // get rid of any preexisting alerts.
+      removeAllAlerts();
+      for (let message in err.message) {
+        alertMessage(err.message[message]);
+      }
+
       console.log(err);
     }
   }
--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,34 +1,28 @@
-const baseURL = import.meta.env.VITE_SERVER_URL
-
-function convertToJson(res) {
+const baseURL = "http://server-nodejs.cit.byui.edu:3000/";
+async function convertToJson(res) {
+  const data = await res.json();
   if (res.ok) {
-    return res.json();
+    return data;
   } else {
-    throw new Error("Bad Response");
+    throw { name: "servicesError", message: data };
   }
 }
 
-
 export default class ExternalServices {
-  /*
   constructor(category) {
-    this.category = category;
-    this.path = `../json/${this.category}.json`;
+    // this.category = category;
+    // this.path = `../json/${this.category}.json`;
   }
-    */
-
-async getData(category) {
+  async getData(category) {
     const response = await fetch(baseURL + `products/search/${category}`);
     const data = await convertToJson(response);
     return data.Result;
-}
-
+  }
   async findProductById(id) {
     const response = await fetch(baseURL + `product/${id}`);
     const data = await convertToJson(response);
     return data.Result;
   }
-
   async checkout(payload) {
     const options = {
       method: "POST",
@@ -38,22 +32,5 @@ async getData(category) {
       body: JSON.stringify(payload),
     };
     return await fetch(baseURL + "checkout/", options).then(convertToJson);
-  }
-}
-
-export function searchTents() {
-  const input = document.getElementById("myInput");
-  const filter = input.value.toUpperCase();
-  const ul = document.getElementById("list");
-  const li = ul.getElementsByTagName("li");
-
-  for (let i = 0; i < li.length; i++) {
-    const a = li[i].getElementsByTagName("a")[0];
-    const txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "block";
-    } else {
-      li[i].style.display = "none";
-    }
   }
 }
