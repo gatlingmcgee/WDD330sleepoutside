4ac4fe7e24c90c6ff8a40b07d2dded882202717f
AndrewJHalisky <ajhalisky@gmail.com>
2024-11-30 19:22:22 -0500

    Changed ProductData to ExternalServices

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -6,7 +6,7 @@ import { loadHeaderFooter, getParam } from "./utils.mjs";
 loadHeaderFooter();
 const category = getParam("category");
 // first create an instance of our ExternalServices class.
-const dataSource = new ProductData();
+const dataSource = new ExternalServices();
 // then get the element we want the product list to render in
 const listElement = document.querySelector(".product-list");
 // then create an instance of our ProductList class and send it the correct information.
